0a437701e1d14dd7c9cd6de4caa66128d51e42cc
Aaron Haurwitz <aaron.haurwitz@gmail.com>
2020-07-27 14:02:56 -0400

    Revert "switch space and layers around"
    This reverts commit c17db175e8590d46c01c92c7e8f1b7a5f22c7d5a.

--- keyboards/lily58/keymaps/ahaurw01/keymap.c
+++ keyboards/lily58/keymaps/ahaurw01/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |Enter |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |LOWER | LAlt | LGUI | /Space  /       \Space \  |RAISE |   =  |   \  |
+ *                   | LAlt | LGUI |Space | /Lower  /       \Raise \  |Space |   =  |   \  |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_ENT, \
-                             LOWER, KC_LALT, KC_LGUI, KC_SPC,  KC_SPC, RAISE,  KC_EQL,  KC_BSLS \
+                             KC_LALT, KC_LGUI,KC_SPC, LOWER,   RAISE,   KC_SPC,   KC_EQL,  KC_BSLS \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------|    |-------|   _  |   -  |   +  |   {  |   }  |   |  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |LOWER | LAlt | LGUI | /Space  /       \Space \  |RAISE |   =  |   \  |
+ *                   | LAlt | LGUI |Space | /Lower  /       \Raise \  |Space |   =  |   \  |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -83,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------|    |-------|   +  |   -  |   =  |   [  |   ]  |   \  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |LOWER | LAlt | LGUI | /Space  /       \Space \  |RAISE |   =  |   \  |
+ *                   | LAlt | LGUI |Space | /Lower  /       \Raise \  |Space |   =  |   \  |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -105,7 +105,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * | Reset|EReset|      |      |      |      |-------|    |-------|      |      | MODE | HUE- | SAT- | VAL- |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |LOWER | LAlt | LGUI | /Space  /       \Space \  |RAISE |   =  |   \  |
+ *                   | LAlt | LGUI |Space | /Lower  /       \Raise \  |Space |   =  |   \  |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
