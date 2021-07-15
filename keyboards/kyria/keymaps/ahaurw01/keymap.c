6c24568ece42a5bc4b5795801f4b690f5062c7e0
Aaron Haurwitz <aaron.haurwitz@gmail.com>
2021-07-15 14:41:50 -0400

    even more

--- keyboards/kyria/keymaps/ahaurw01/keymap.c
+++ keyboards/kyria/keymaps/ahaurw01/keymap.c
@@ -31,14 +31,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
      * | LShift |   Z  |   X  |   C  |   V  |   B  |  `   | Lower|  | VolDn| VolUp|   N  |   M  | ,  < | . >  | /  ? |  Ent   |
      * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
-     *                        | Lower| Ctrl | Cmd  | Space| Lower|  | Raise| Space|   =  |   \  | Raise|
+     *                        | Ctrl | Cmd  | Space| Lower| Lower|  | Raise| Space| Space|   =  |   \  |
      *                        `----------------------------------'  `----------------------------------'
      */
     [_QWERTY] = LAYOUT(
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
         CTL_T(KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_GRV, LOWER, KC_VOLD, KC_VOLU, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ENT,
-        LOWER, KC_LALT, KC_LGUI, KC_SPC, LOWER, RAISE, KC_SPC, KC_EQL, KC_BSLS, RAISE
+        KC_LALT, KC_LGUI, KC_SPC, LOWER, LOWER, RAISE, KC_SPC, KC_SPC, KC_EQL, KC_BSLS
     ),
     /*
      * Lower Layer
